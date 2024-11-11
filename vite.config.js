08d111f35704aeb80c487e72ee19c301dde44d6a
Shakil Nawaz <shakilnwz@gmail.com>
2024-11-11 16:06:06 +0600

    deploy yml added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import deno from '@deno/vite-plugin'
+import { defineConfig } from "vite";
+import deno from "@deno/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [deno()],
-})
+  base: "/shakilnwz.github.io/",
+});
