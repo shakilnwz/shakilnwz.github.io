0249e55726c54e63479d2605e36dc68522f8131a
Shakil Nawaz <shakilnwz@gmail.com>
2024-11-11 19:15:49 +0600

    added logo

--- src/main.js
+++ src/main.js
@@ -1,25 +1,14 @@
 import "./style.css";
-import javascriptLogo from "./assets/javascript.svg";
+import logo from "./assets/logo.svg";
 import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
-  <div>
-    <img src="/vite-deno.svg" alt="Vite with Deno" />
-    <div>
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-        <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-      </a>
-    </div>
-    <h1>Hello Vite!</h1>
-    <div class="card">
+  <div class="flex flex-col items-center max-w-2xl mx-auto bg-gray-50">
+    <img src="${logo}" alt="snz logo" />
+    <h1>Hello World!</h1>
+    <div class="w-fit border-gray-400 border rounded-lg px-4 py-2 my-2">
       <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `;
 
