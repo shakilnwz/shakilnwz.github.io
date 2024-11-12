8efa4120f599c786666832b02e0405d7f19a52fa
Shakil Nawaz <shakilnwz@gmail.com>
2024-11-12 10:25:12 +0600

    added logo

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import "./style.css";
-import logo from "./assets/logo.svg";
+import logo from "./assets/logo_alt.svg";
 import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
-  <div class="flex flex-col items-center max-w-2xl mx-auto bg-gray-50">
+  <div class="flex flex-col text-gray-200 items-center mx-auto">
     <img src="${logo}" alt="snz logo" />
     <h1>Hello World!</h1>
     <div class="w-fit border-gray-400 border rounded-lg px-4 py-2 my-2">
