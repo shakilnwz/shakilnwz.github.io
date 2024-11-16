3bee13be3eff2e2010716204b9b69b467a3156ce
Shakil Nawaz <shakilnwz@gmail.com>
2024-11-17 00:32:09 +0600

    nav changed

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { setupCounter } from "./counter.js";
 import navbar from "./navbar.js";
 document.querySelector("#app").innerHTML = `
     <nav id="navbar"></nav>
-  <div class="max-w-3xl flex flex-col text-gray-200 items-center mx-auto">
+  <div class="max-w-6xl h-[120vh] flex flex-col text-gray-200 items-center mx-auto">
     <h1>Hello World!</h1>
     <div class="w-fit border-gray-400 border rounded-lg px-4 py-2 my-2">
       <button id="counter" type="button"></button>
--- src/navbar.js
+++ src/navbar.js
@@ -1,9 +1,29 @@
 import logo from "./assets/logo_alt.svg";
 export default function (element) {
-  element.classList.add("py-2", "bg-white", "sticky-top", "bg-opacity-80");
+  element.classList.add("relative");
   element.innerHTML = `
-    <div class="max-w-4xl mx-auto flex flex-row justify-between items-center">
-        <a class="max-w-20 block" href="/">
+    <div 
+        class="
+        relative
+        after:w-8
+        after:-right-8
+        after:rounded-tl-2xl
+        after:h-8
+        after:absolute
+        after:top-0
+        after:-z-10
+        after:shadow-[-16px_0px_0px_0px_white]
+        before:w-8
+        before:-left-8
+        before:rounded-tr-2xl
+        before:h-8
+        before:absolute
+        before:top-0
+        before:-z-10
+        before:shadow-[16px_0px_0px_0px_white]
+        p-2 bg-white max-w-6xl mx-auto flex flex-row justify-between items-center rounded-b-xl z-20
+        ">
+        <a class="max-w-20 block self-start" href="/">
             <img src='${logo}' alt="logo"/>
         </a>
         <div class="flex-row flex gap-10">
