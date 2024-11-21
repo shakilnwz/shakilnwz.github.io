99c399e2332eab83a839405e9b45a83edcd26dcb
Shakil Nawaz <shakilnwz@gmail.com>
2024-11-22 00:03:24 +0600

    changed nav style

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import "./style.css";
 import { setupCounter } from "./counter.js";
 import navbar from "./navbar.js";
-document.querySelector("#app").innerHTML = `
+const main = document.querySelector("#app");
+main.innerHTML = `
     <nav id="navbar"></nav>
   <div class="max-w-6xl h-[120vh] flex flex-col text-gray-200 items-center mx-auto">
     <h1>Hello World!</h1>
@@ -10,6 +11,6 @@ document.querySelector("#app").innerHTML = `
     </div>
   </div>
 `;
-
+main.classList.add("bg-ctp-base");
 setupCounter(document.querySelector("#counter"));
 navbar(document.querySelector("#navbar"));
--- src/navbar.js
+++ src/navbar.js
@@ -4,29 +4,29 @@ export default function (element) {
   element.innerHTML = `
     <div 
         class="
-        relative
-        after:w-8
-        after:-right-8
-        after:rounded-tl-2xl
-        after:h-8
-        after:absolute
-        after:top-0
-        after:-z-10
-        after:shadow-[-16px_0px_0px_0px_white]
-        before:w-8
-        before:-left-8
-        before:rounded-tr-2xl
-        before:h-8
-        before:absolute
-        before:top-0
-        before:-z-10
-        before:shadow-[16px_0px_0px_0px_white]
-        p-2 bg-white max-w-6xl mx-auto flex flex-row justify-between items-center rounded-b-xl z-20
+            relative
+            after:w-8
+            after:-right-8
+            after:rounded-tl-2xl
+            after:h-8
+            after:absolute
+            after:top-0
+            after:-z-10
+            after:shadow-[-16px_0px_0px_0px_white]
+            before:w-8
+            before:-left-8
+            before:rounded-tr-2xl
+            before:h-8
+            before:absolute
+            before:top-0
+            before:-z-10
+            before:shadow-[16px_0px_0px_0px_white]
+            p-2 bg-white max-w-6xl mx-auto flex flex-row justify-between items-center rounded-b-xl z-20
         ">
         <a class="max-w-20 block self-start" href="/">
             <img src='${logo}' alt="logo"/>
         </a>
-        <div class="flex-row flex gap-10">
+        <div class="flex-row flex gap-10 text-pink">
             <a href="#projects">Projects</a>
             <a href="#about">About</a>
             <a href="#contact">Contact</a>
