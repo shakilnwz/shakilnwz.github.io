0451f4e9745be080b4213fa4da945781f8056fda
Shakil Nawaz <shakilnwz@gmail.com>
2024-11-22 00:03:02 +0600

    added catppuccin pallete for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import catppuccin from "@catppuccin/tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    catppuccin({
+      // prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
+      // default is `false`, which means no prefix
+      prefix: "ctp",
+      // which flavour of colours to use by default, in the `:root`
+      defaultFlavour: "latte",
+    }),
+  ],
 };
