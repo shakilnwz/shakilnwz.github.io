eba1a924a92e98ace189b352f94a4b37c2637dc4
Shakil Nawaz <shakilnwz@gmail.com>
2024-11-26 21:55:27 +0600

    added white theme

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import navbar from "./navbar.js";
 const main = document.querySelector("#app");
 main.innerHTML = `
     <nav id="navbar"></nav>
-  <div class="max-w-6xl h-[120vh] flex flex-col text-gray-200 items-center mx-auto">
+  <div class="max-w-6xl h-[120vh] flex flex-col text-ctp-pink items-center mx-auto">
     <h1>Hello World!</h1>
-    <div class="w-fit border-gray-400 border rounded-lg px-4 py-2 my-2">
+    <div class="w-fit border-ctp-pink border rounded-lg px-4 py-2 my-2">
       <button id="counter" type="button"></button>
     </div>
   </div>
 `;
-main.classList.add("bg-ctp-base");
+main.classList.add("bg-gradient-to-b", "from-ctp-base", "to-ctp-crust");
 setupCounter(document.querySelector("#counter"));
 navbar(document.querySelector("#navbar"));
--- src/navbar.js
+++ src/navbar.js
@@ -1,9 +1,6 @@
 import logo from "./assets/logo_alt.svg";
 export default function (element) {
-  element.classList.add("relative");
-  element.innerHTML = `
-    <div 
-        class="
+  const outerRoundNav = `
             relative
             after:w-8
             after:-right-8
@@ -12,7 +9,7 @@ export default function (element) {
             after:absolute
             after:top-0
             after:-z-10
-            after:shadow-[-16px_0px_0px_0px_white]
+            after:shadow-[-16px_0px_0px_0px_ctp-flamingo]
             before:w-8
             before:-left-8
             before:rounded-tr-2xl
@@ -20,13 +17,17 @@ export default function (element) {
             before:absolute
             before:top-0
             before:-z-10
-            before:shadow-[16px_0px_0px_0px_white]
-            p-2 bg-white max-w-6xl mx-auto flex flex-row justify-between items-center rounded-b-xl z-20
-        ">
+            before:shadow-[16px_0px_0px_0px_ctp-flamingo]
+            p-2 bg-ctp-flamingo max-w-6xl mx-auto flex flex-row justify-between items-center rounded-b-xl z-20
+        `;
+  element.classList.add("relative");
+  element.innerHTML = `
+    <div 
+        class="${outerRoundNav}">
         <a class="max-w-20 block self-start" href="/">
             <img src='${logo}' alt="logo"/>
         </a>
-        <div class="flex-row flex gap-10 text-pink">
+        <div class="flex-row flex gap-10 text-ctp-pink">
             <a href="#projects">Projects</a>
             <a href="#about">About</a>
             <a href="#contact">Contact</a>
