e336270ba72b45e909c039417e81e92d5f6ab0a0
Shakil Nawaz <shakilnwz@gmail.com>
2024-11-29 00:48:58 +0600

    added tailwind v4 beta support

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
 import "./style.css";
+import javascriptLogo from "./assets/javascript.svg";
 import { setupCounter } from "./counter.js";
-import navbar from "./navbar.js";
-const main = document.querySelector("#app");
-main.innerHTML = `
-    <nav id="navbar"></nav>
-  <div class="max-w-6xl h-[120vh] flex flex-col text-ctp-pink items-center mx-auto">
-    <h1>Hello World!</h1>
-    <div class="w-fit border-ctp-pink border rounded-lg px-4 py-2 my-2">
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <img src="/vite-deno.svg" alt="Vite with Deno" />
+    <div>
+      <a href="https://vite.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+        <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+      </a>
+    </div>
+    <h1>Hello Vite!</h1>
+    <div class="card">
       <button id="counter" type="button"></button>
     </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
   </div>
 `;
-main.classList.add("bg-gradient-to-b", "from-ctp-base", "to-ctp-crust");
+
 setupCounter(document.querySelector("#counter"));
-navbar(document.querySelector("#navbar"));
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import deno from "@deno/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno()],
+  plugins: [deno(), tailwindcss()],
 });
