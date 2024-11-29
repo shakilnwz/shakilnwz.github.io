aa831db9846c594332d8b640849b9e784763ba22
Shakil Nawaz <shakilnwz@gmail.com>
2024-11-29 19:08:35 +0600

    added tailwindcss v4 beta

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import deno from "@deno/vite-plugin";
+import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno(), tailwindcss()],
+  plugins: [deno(), vue(), tailwindcss()],
 });
