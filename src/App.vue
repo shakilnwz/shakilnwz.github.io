482fb129cd74de99038a4ae42df404970624f447
Shakil Nawaz <shakilnwz@gmail.com>
2024-12-18 09:57:10 +0600

    tab complete

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@ import Nav from './components/Nav.vue'
 </script>
 
 <template>
-    <div className="mx-auto font-display bg-gradient-to-b from-base-x to-base-y">
+    <div className="mx-auto text-brand-x font-display bg-gradient-to-b from-base-x to-base-y">
         <Nav />
         <h1 className="text-transparent w-fit bg-gradient-to-r from-brand-y to-brand-x bg-clip-text text-6xl font-bold">Hello World!</h1>
-    </div></template>
+    </div>
+</template>
 
 <style scoped>
 
---
+++ src/components/NavLink.vue
@@ -0,0 +1,22 @@
+<script setup>
+const link = defineProps(
+    {
+        "href": String,
+        "svgd": String,
+        "text": String
+
+    }
+)
+</script>
+<template>
+    <a class="justify-center flex flex-row flex-wrap sm:flex-nowrap items-center gap-2 hover:bg-brand-y hover:text-surface sm:px-5 py-4 sm:py-1" :href="link.href">
+        <svg class="fill-current h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path
+                fill-rule="evenodd"
+                :d="link.svgd"
+                clip-rule="evenodd"
+            />
+        </svg>
+        {{ link.text }}
+    </a>
+</template>
