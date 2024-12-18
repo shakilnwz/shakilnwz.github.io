7941532e82d1c789d19875bbf8a0614b90c67690
Shakil Nawaz <shakilnwz@gmail.com>
2024-12-18 12:45:22 +0600

    added main

---
+++ src/components/Main.vue
@@ -0,0 +1,9 @@
+<template>
+    <main class='max-w-5xl mx-auto px-5 sm:px-8'>
+        <section>
+
+            <h1 class="text-transparent w-fit bg-gradient-to-r from-brand-y to-brand-x bg-clip-text text-6xl font-bold">Shakil Nawaz</h1>
+
+        </section>
+    </main>
+</template>
