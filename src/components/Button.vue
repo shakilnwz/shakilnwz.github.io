9da7b4897d24f5a65919bfc2c5d3ee5fb7455474
Shakil Nawaz <shakilnwz@gmail.com>
2024-12-19 00:35:21 +0600

    added cta button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,7 +1,7 @@
 <script setup>
 const link = defineProps(
     {
-        "btype": String,
+        "secondary": Boolean,
         "href": String,
         "svgd": String,
         "text": String
@@ -11,8 +11,18 @@ const link = defineProps(
 
 </script>
 <template>
-    <a class="justify-center font-bold rounded-xl border-brand-y border-2 w-fit flex flex-row flex-wrap items-center gap-x-2 bg-brand-y hover:bg-surface text-surface hover:text-brand-y sm:px-5 py-4 sm:py-1.5" :href="link.href">
-        <svg class="fill-current h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+    <a v-if="link.secondary" class="justify-center font-bold rounded-xl border-brand-y border-2 w-fit flex flex-row flex-wrap items-center gap-x-2 hover:bg-brand-y bg-surface hover:text-surface text-brand-y px-5 py-1" :href="link.href">
+        <svg v-if="link.svgd" class="fill-current h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path
+                fill-rule="evenodd"
+                :d="link.svgd"
+                clip-rule="evenodd"
+            />
+        </svg>
+        {{ link.text }}
+    </a>
+    <a v-else class="justify-center font-bold rounded-xl border-brand-y border-2 w-fit flex flex-row flex-wrap items-center gap-x-2 bg-brand-y hover:bg-surface text-surface hover:text-brand-y px-5 py-1" :href="link.href">
+        <svg v-if="link.svgd" class="fill-current h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path
                 fill-rule="evenodd"
                 :d="link.svgd"
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,9 +3,8 @@ import Button from "./Button.vue"
 </script>
 
 <template>
-    <main class='max-w-5xl mx-auto px-5  sm:px-8'>
-        <section class="grid sm:grid-cols-3  gap-2 sm:gap-0">
-
+    <main class='max-w-5xl mx-auto px-5 sm:px-8 '>
+        <section class="grid sm:grid-cols-3  gap-2 sm:gap-0 sm:py-10 py-5">
             <div class="sm:col-span-2">
                 <h1 class="text-brand-y text-xl">
                     Hello World! I'm
@@ -29,10 +28,21 @@ import Button from "./Button.vue"
                 <p>
                     I have skills with WordPress, Elementor, Breakdance Builder, TailwindCSS, Semantic HTML and CSS. Currently exploring Laravel.
                 </p>
-                <Button text="More About Me" href="/about" svgd="M12 3.514c0 1.94-1.79 3.514-4 3.514S4 5.454 4 3.514C4 1.574 5.79 0 8 0s4 1.573 4 3.514ZM0 13.122c0-2.912 2.85-5.271 6.368-5.271h3.264C13.15 7.85 16 10.21 16 13.122c0 .485-.475.878-1.06.878H1.06C.476 14 0 13.607 0 13.122Z"/>
+
+                <div class="flex flex-row gap-2">
+                    <Button 
+                        text="More About Me" 
+                        href="/about" 
+                        svgd="M12 3.514c0 1.94-1.79 3.514-4 3.514S4 5.454 4 3.514C4 1.574 5.79 0 8 0s4 1.573 4 3.514ZM0 13.122c0-2.912 2.85-5.271 6.368-5.271h3.264C13.15 7.85 16 10.21 16 13.122c0 .485-.475.878-1.06.878H1.06C.476 14 0 13.607 0 13.122Z"
+                    />
+                    <Button
+                        secondary
+                        text="Résumé" 
+                        href="/resume" 
+                        svgd="M0 1.75C0 .785.926 0 2.065 0h5.16v3.5c0 .484.462.875 1.033.875h4.13v3.938h-6.71c-1.14 0-2.065.784-2.065 1.75V14H2.065C.925 14 0 13.215 0 12.25V1.75ZM12.387 3.5H8.258V0l4.13 3.5ZM6.71 9.625H5.677c-.283 0-.516.197-.516.438v3.5c0 .24.233.437.516.437.284 0 .517-.197.517-.438v-.874h.516c.996 0 1.806-.687 1.806-1.532s-.81-1.531-1.806-1.531Zm0 2.188c.429 0 .774-.293.774-.657 0-.363-.345-.656-.774-.656h-.516v1.313h.516Zm4.129-2.188H9.806c-.284 0-.516.197-.516.438v3.5c0 .24.232.437.516.437h1.033c.854 0 1.548-.588 1.548-1.313v-1.75c0-.724-.694-1.312-1.548-1.312Zm0 3.5c.284 0 .516-.197.516-.438v-1.75c0-.24-.232-.437-.516-.437h-.516v2.625h.516Zm3.096-3.5c-.283 0-.516.197-.516.438v3.5c0 .24.233.437.516.437.284 0 .517-.197.517-.438V12.25h1.032c.284 0 .516-.197.516-.438 0-.24-.232-.437-.516-.437h-1.032V10.5h1.032c.284 0 .516-.197.516-.438 0-.24-.232-.437-.516-.437h-1.549Z"
+                    />
+                </div>
             </div>
-
-
         </section>
     </main>
 </template>
