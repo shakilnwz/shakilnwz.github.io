58b6b60467c3df0f72810bf5480fb4b46ba7aec3
Shakil Nawaz <shakilnwz@gmail.com>
2024-12-23 14:06:04 +0600

    added articles section

--- src/Main.vue
+++ src/Main.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Hero from './sections/Hero.vue'
 import Journey from './sections/Journey.vue'
+import Articles from './sections/Articles.vue'
 </script>
 
 <template>
     <main>
         <Hero />
         <Journey />
+        <Articles />
     </main>
 </template>
 
---
+++ src/sections/Articles.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Section from '../components/Section.vue'
+</script>
+
+<template>
+    <Section class="space-y-2.5 sm:space-y-7 relative">
+        <h2 class="sticky top-0 z-10 backdrop-blur-2xl sm:top-20">Recent Articles</h2>
+        <div class="flex flex-col sm:flex-row flex-wrap sm:gap-7 gap-4">
+            <p class="sm:max-w-70">I have skills with WordPress, Elementor, Breakdance Builder, TailwindCSS, Semantic HTML and CSS. Currently exploring Laravel.</p>
+
+            <div class="grow relative space-y-11">
+
+                <!-- skill card -->
+                
+
+            </div>
+        </div>
+        
+    </Section>
+</template>
