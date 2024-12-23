22f7aa6c78f7f0c9da4e324aa9cb50e4b9efe19f
Shakil Nawaz <shakilnwz@gmail.com>
2024-12-23 22:00:21 +0600

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [deno(), vue(), tailwindcss()],
+  base: "/shakilnwz.github.io/",
 });
