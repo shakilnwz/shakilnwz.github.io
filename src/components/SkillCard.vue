f141dbdbff736ce3280d82ba7ecd06a69ae2e19a
Shakil Nawaz <shakilnwz@gmail.com>
2024-12-23 14:05:49 +0600

    added skills section in journey

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -1,21 +1,34 @@
 <script setup>
-
+const data = defineProps({
+    'iconSrc' : String,
+    'iconAlt' : String,
+    'date' : String,
+    'title' : String,
+    'summary' : String
+})
 </script>
 
 <template>
-    <div class='flex flex-row gap-4 transition-all duration-500 z-10 relative'>
+    <div class='flex flex-row gap-4 transition-all duration-500 relative'>
         <!-- svg icon -->
         <div class="w-17 h-17 sm:w-25 sm:h-25 shrink-0 bg-surface rounded-full shadow-brand-y shadow-sm grid place-items-center transition-all duration-500">
-            <img class="w-10 sm:w-13" src="../assets/vue.svg" alt="vue logo">
+            <img 
+                class="w-10 h-10 sm:h-12 sm:w-13" 
+                :src="data.iconSrc" 
+                :alt="data.iconAlt"
+                loading="lazy"
+            >
         </div>
         <!-- skill detail -->
-        <div class="flex flex-col justify-center grow relative">
-            <p class="text-sm sm:text-base">Jan 2022</p>
-            <p class="text-sm sm:text-base font-bold">My post title looooong</p>
-            <p class="text-sm sm:text-base">I love to explore technologies.</p>
-            <svg xmlns="http://www.w3.org/2000/svg"  class='w-full h-0.5 stroke-current text-brand-y absolute bottom-0'>
+        <div class="flex flex-col justify-center grow relative gap-0 sm:gap-1">
+            <p class="text-sm sm:text-base">{{ data.date }}</p>
+            <p class="text-sm sm:text-base font-bold">{{ data.title }}</p>
+
+            <!-- divider -->
+            <svg xmlns="http://www.w3.org/2000/svg"  class='w-full h-0.5 stroke-current text-brand-y'>
                 <line x1="0"  x2="100%" fill="transparent" stroke-width="4px" stroke-dasharray="12 5 3 5" />
             </svg>
+            <p class="text-sm sm:text-base">{{ data.summary }}</p>
         </div>
     </div>
 </template>
--- src/sections/Journey.vue
+++ src/sections/Journey.vue
@@ -1,11 +1,12 @@
 <script setup>
 import Section from '../components/Section.vue'
 import SkillCard from '../components/SkillCard.vue'
+import journey from '../assets/journey.json'
 </script>
 
 <template>
     <Section class="space-y-2.5 sm:space-y-7 relative">
-        <h2 class="sticky top-0 backdrop-blur-2xl sm:top-20">Journey</h2>
+        <h2 class="sticky top-0 z-10 backdrop-blur-2xl sm:top-20">Journey</h2>
         <div class="flex flex-col sm:flex-row flex-wrap sm:gap-7 gap-4">
             <p class="sm:max-w-70">I have skills with WordPress, Elementor, Breakdance Builder, TailwindCSS, Semantic HTML and CSS. Currently exploring Laravel.</p>
 
@@ -13,13 +14,15 @@ import SkillCard from '../components/SkillCard.vue'
                 <span class="absolute h-full w-1 bg-brand-y top-0 left-8 sm:left-12 z-0"></span>
 
                 <!-- skill card -->
-                <SkillCard />
-                <SkillCard />
-                <SkillCard />
-                <SkillCard />
-                <SkillCard />
-                <SkillCard />
-                <SkillCard />
+                <SkillCard
+                    v-for='item in journey'
+                    :key="item.id"
+                    :iconSrc= "item.iconSrc"
+                    :iconAlt="item.iconAlt"
+                    :date="item.date"
+                    :title="item.title"
+                    :summary="item.summary"
+                />
 
             </div>
         </div>
