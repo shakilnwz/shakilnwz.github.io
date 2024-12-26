284b309d272daa5a67f15b33d82282685abd31a4
Shakil Nawaz <shakilnwz@gmail.com>
2024-12-26 18:24:33 +0600

    added hero animation

--- src/Main.vue
+++ src/Main.vue
@@ -3,12 +3,10 @@ import Hero from './sections/Hero.vue'
 import Journey from './sections/Journey.vue'
 import Articles from './sections/Articles.vue'
 import Projects from './sections/Projects.vue'
-import HeroX from './sections/HeroX.vue'
 </script>
 
 <template>
     <main>
-        <HeroX />
         <Hero />
         <Journey />
         <Articles />
--- src/sections/Hero.vue
+++ src/sections/Hero.vue
@@ -1,55 +1,95 @@
 <script setup>
-import { inject } from 'vue'
+import { ref, inject } from 'vue'
 import Section from '../components/Section.vue'
 import Button from "../components/Button.vue"
 
 const iconList = inject('icons');
+const cursorPos = {
+    'style' : ref(null)
+}
+
+const pointer = (e)=>{
+    e.stopImmediatePropagation();
+    cursorPos.style.value = `left: ${e.clientX - 160}px`
+}
+
 </script>
 
 <template>
-    <Section contained class="grid sm:grid-cols-3 gap-2 sm:gap-0">
-        <div class="sm:col-span-2">
-            <h1 class="text-brand-y text-lg sm:text-xl">
-                Hello World! I'm
-                <span class="text-transparent w-fit bg-gradient-to-r from-brand-y to-brand-x bg-clip-text text-5xl sm:text-6xl font-bold block">
-                    Shakil Nawaz
-                </span>
-            </h1>
-            <p class="text-lg sm:text-xl font-bold">A web developer, Specialized in JavaScript and PHP</p>
-        </div>
+    <Section id="hero" @mousemove="pointer" class="relative overflow-hidden group">
+        <span id="fancybox" class="z-0 absolute  top-0 left-0 right-0 bottom-0">
+            <span id="cursorpulse"  :style="cursorPos.style.value" class="transition-all rounded-3xl duration-300 top-1/4 sm:top-0 group-hover:w-80 w-full h-50 sm:h-full absolute bg-gradient-to-r from-transparent via-brand-y/70 to-transparent"></span>
+        </span>
+        <div class="relative z-10 max-w-5xl px-5 sm:px-8 mx-auto grid sm:grid-cols-3 gap-2 sm:gap-0">
 
-        <div class="sm:row-span-2 sm:justify-self-end justify-self-center self-center">
-            <div class="bg-radial from-brand-x/90 to-brand-y/30 to-45% max-w-60 aspect-ratio-1 rounded-full shadow-[0_0_9px_rgba(0,0,0,0.5)]">
-                <img
-                    class="w-60 object-contain"
-                    src="../assets/avatar.webp"
-                    alt="avatar"
-                />
+            <div class="sm:col-span-2">
+                <h1 class="text-brand-y text-lg sm:text-xl">
+                    Hello World! I'm
+                    <span class=" text-transparent w-fit bg-gradient-to-r from-brand-y to-brand-x bg-clip-text text-5xl sm:text-6xl font-bold block">
+                        Shakil Nawaz
+                    </span>
+                </h1>
+                <p class="text-lg sm:text-xl font-bold">A web developer, Specialized in JavaScript and PHP</p>
             </div>
-        </div> 
 
-        <div class="py-5 space-y-4 sm:col-span-2">
-            <p>
-                I love to explore newer technologies and build things upon that. Also love to build user centric websites that actually helps.
-            </p>
-            <p>
-                I have skills with WordPress, Elementor, Breakdance Builder, TailwindCSS, Semantic HTML and CSS. Currently exploring Laravel.
-            </p>
+            <div class="sm:row-span-2 sm:justify-self-end justify-self-center place-self-center">
+                <div 
+                    class="bg-radial from-brand-x/90 to-brand-y/30 to-45% max-w-60 aspect-square rounded-full shadow-[0_0_9px_rgba(0,0,0,0.5)]">
 
-            <div class="flex flex-row gap-2 flex-wrap">
-                <Button
-                    text="More About Me"
-                    href="/about"
-                    :svgd="iconList.about"
-                />
-                <Button
-                    secondary
-                    text="Résumé"
-                    href="/resume"
-                    :svgd="iconList.pdf"
-                />
+                    <img
+                        class="relative z-10 w-60 object-contain"
+                        src="../assets/avatar.webp"
+                        alt="avatar"
+                    />
+                </div>
+            </div> 
+
+            <div class="py-5 space-y-4 sm:col-span-2">
+                <p>
+                    I love to explore newer technologies and build things upon that. Also love to build user centric websites that actually helps.
+                </p>
+                <p>
+                    I have skills with WordPress, Elementor, Breakdance Builder, TailwindCSS, Semantic HTML and CSS. Currently exploring Laravel.
+                </p>
+
+                <div class="flex flex-row gap-2 flex-wrap">
+                    <Button
+                        text="More About Me"
+                        href="/about"
+                        :svgd="iconList.about"
+                    />
+                    <Button
+                        secondary
+                        text="Résumé"
+                        href="/resume"
+                        :svgd="iconList.pdf"
+                    />
+                </div>
             </div>
+
         </div>
     </Section>
 
 </template>
+<style scoped>
+#cursorpulse{
+    animation: run 5s linear infinite;
+}
+#hero:hover #cursorpulse{
+    animation: none;
+}
+@keyframes run {
+from{
+    transform: translateX(-100%);
+}
+to {
+    transform: translateX(100%);
+}
+}
+
+#fancybox{
+    mask-image: url('../assets/bgfull.svg');
+    mask-size: 70%;
+    mask-position: center;
+}
+</style>
