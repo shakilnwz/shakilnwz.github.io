12b1cc6728ee54c2755fbb80ae9a88c8d508bd2a
Shakil Nawaz <shakilnwz@gmail.com>
2025-01-13 00:09:37 +0600

    updated contact page

--- src/components/templates/PrimaryFooter.vue
+++ src/components/templates/PrimaryFooter.vue
@@ -7,65 +7,42 @@ const iconList = inject('icons')
 const route = inject('route')
 </script>
 <template>
-    <footer :class="{'print:hidden': route.path === '/resume'}">
-        <div class="mx-auto px-5 sm:px-8 sm:py-10 py-5 max-w-5xl space-y-2.5 sm:space-y-4">
+    <footer :class="{ 'print:hidden': route.path === '/resume' }">
+        <div :class="{
+            'mx-auto px-5 sm:px-8 sm:py-10 py-5 max-w-5xl space-y-2.5 sm:space-y-4': true,
+            'hidden': route.path === '/contact'
+        }">
             <h2 class="py-2">Connect</h2>
             <p>Let's get connected and plan on building your next dream WebSite.</p>
             <!-- social links with text -->
             <ul class="space-y-2.5 sm:space-y-4 ">
                 <li>
-                    <SocialLink
-                        text="Email me at"
-                        textHighlight="shakilnwz@gmail.com"
-                        href="mailto:shakilnwz@gmail.com"
-                        :svgd="iconList.email"
-                        ariaLebel="Email me at shakilnwz@gmail.com"
-                    />
+                    <SocialLink text="Email me at" textHighlight="shakilnwz@gmail.com" href="mailto:shakilnwz@gmail.com"
+                        :svgd="iconList.email" ariaLebel="Email me at shakilnwz@gmail.com" />
                 </li>
                 <li>
-                    <SocialLink
-                        text="Let's connect on"
-                        textHighlight="WhatsApp"
-                        href="https://wa.me/8801791907711"
-                        :svgd="iconList.whatsapp"
-                        ariaLebel="Let's connect on whatsapp"
-                    />
+                    <SocialLink text="Let's connect on" textHighlight="WhatsApp" href="https://wa.me/8801791907711"
+                        :svgd="iconList.whatsapp" ariaLebel="Let's connect on whatsapp" />
                 </li>
                 <li>
-                    <SocialLink
-                        text="Find me on"
-                        textHighlight="Discord"
-                        href="https://discord.com/channels/@me/shakilnwz"
-                        :svgd="iconList.discord"
-                        ariaLebel="find me on discord"
-                    />
+                    <SocialLink text="Find me on" textHighlight="Discord"
+                        href="https://discord.com/channels/@me/shakilnwz" :svgd="iconList.discord"
+                        ariaLebel="find me on discord" />
                 </li>
             </ul>
 
             <!-- social icons -->
             <div class="flex flex-row gap-2.5 py-2.5">
-                <SocialLink
-                    icon
-                    href="https://www.linkedin.com/in/shakilnwz/"
-                    :svgd="iconList.linkedin"
-                    ariaLebel="connect on linkedin"
-                />
-                <SocialLink
-                    icon
-                    href="https://github.com/shakilnwz"
-                    :svgd="iconList.github"
-                    ariaLebel="view my github"
-                />
-                <SocialLink
-                    icon
-                    href="https://x.com/shakilnwz"
-                    :svgd="iconList.twitter"
-                    ariaLebel="find me on x"
-                />
+                <SocialLink icon href="https://www.linkedin.com/in/shakilnwz/" :svgd="iconList.linkedin"
+                    ariaLebel="connect on linkedin" />
+                <SocialLink icon href="https://github.com/shakilnwz" :svgd="iconList.github"
+                    ariaLebel="view my github" />
+                <SocialLink icon href="https://x.com/shakilnwz" :svgd="iconList.twitter" ariaLebel="find me on x" />
             </div>
         </div>
         <div class="relative">
-            <p class="text-base relative z-1 mx-auto px-5 sm:px-8 sm:py-10 py-5 max-w-5xl">&copy; {{ currentYear }} snz</p>
+            <p class="text-base relative z-1 mx-auto px-5 sm:px-8 sm:py-10 py-5 max-w-5xl">&copy; {{ currentYear }} snz
+            </p>
             <div class="z-0 w-full flex flex-row absolute left-0 top-0 h-full" aria-hidden="true">
                 <span class="w-3/5 bg-base-x" aria-hidden="true"></span>
                 <span class="w-[30%] bg-brand-y" aria-hidden="true"></span>
--- src/pages/ContactView.vue
+++ src/pages/ContactView.vue
@@ -1,9 +1,47 @@
 <script setup>
 import Section from '../components/atoms/Section.vue'
+import SocialLink from '../components/atoms/SocialLink.vue';
+import { inject } from 'vue'
+const iconList = inject('icons')
 </script>
 <template>
     <Section contained>
-        <h2>Contact</h2>
+        <h2>Connect</h2>
+        <div :class="{
+            'sm:py-10 py-5 max-w-5xl space-y-2.5 sm:space-y-4': true,
+        }">
+            <p>Let's get connected and plan on building your next dream WebSite.</p>
+            <!-- social links with text -->
+            <ul class="space-y-2.5 sm:space-y-4 ">
+                <li>
+                    <SocialLink text="Email me at" textHighlight="shakilnwz@gmail.com" href="mailto:shakilnwz@gmail.com"
+                        :svgd="iconList.email" ariaLebel="Email me at shakilnwz@gmail.com" />
+                </li>
+                <li>
+                    <SocialLink text="Let's connect on" textHighlight="WhatsApp" href="https://wa.me/8801791907711"
+                        :svgd="iconList.whatsapp" ariaLebel="Let's connect on whatsapp" />
+                </li>
+                <li>
+                    <SocialLink text="Find me on" textHighlight="Discord"
+                        href="https://discord.com/channels/@me/shakilnwz" :svgd="iconList.discord"
+                        ariaLebel="find me on discord" />
+                </li>
+                <li>
+                    <SocialLink text="Let's connect on" textHighlight="LinkedIn"
+                        href="https://www.linkedin.com/in/shakilnwz/" :svgd="iconList.linkedin"
+                        ariaLebel="connect on linkedin" />
+                </li>
+                <li>
+                    <SocialLink text="View My Timeline on" textHighlight="GitHub" href="https://github.com/shakilnwz"
+                        :svgd="iconList.github" ariaLebel="view github" />
+                </li>
+                <li>
+                    <SocialLink text="Find Me On" textHighlight="X" href="https://x.com/shakilnwz"
+                        :svgd="iconList.twitter" ariaLebel="find me on x" />
+                </li>
+            </ul>
+
+        </div>
         <!-- TODO: add contact detail -->
         <!-- TODO: add skype detail -->
         <!-- TODO: add linkedin detail -->
