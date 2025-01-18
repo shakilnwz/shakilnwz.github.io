db3d6849374bd3a362301cd6af36f89fed558a4c
Shakil Nawaz <shakilnwz@gmail.com>
2025-01-18 20:08:00 +0600

    added project

--- src/pages/home/SectionProjects.vue
+++ src/pages/home/SectionProjects.vue
@@ -3,11 +3,11 @@ import { inject } from 'vue';
 import Section from '../../components/atoms/Section.vue'
 import Button from '../../components/atoms/Button.vue';
 import PostLink from '../../components/templates/PostLink.vue';
-import eliteabu from '../../assets/projects/eliteabu.webp';
 import lifkoach from '../../assets/projects/lifkoach.webp';
+import educavo from '../../assets/projects/educavo.webp';
 import projects from '../../assets/projects.json'
 
-const projectThumb = {eliteabu, lifkoach}
+const projectThumb = { educavo, lifkoach }
 
 const iconList = inject('icons')
 
@@ -18,28 +18,16 @@ const iconList = inject('icons')
         <div class="sticky top-0 z-10 backdrop-blur-3xl sm:top-15 gap-2 ">
             <div class="max-w-5xl px-5 sm:px-8 mx-auto flex flex-wrap justify-between py-2 items-center">
                 <h2>Recent Projects</h2>
-                <Button 
-                    secondary
-                    href="/projects"
-                    :svgd="iconList.projects"
-                    text="Show All"
-                />
+                <Button secondary href="/projects" :svgd="iconList.projects" text="Show All" />
             </div>
         </div>
         <div class="max-w-5xl px-5 sm:px-8 mx-auto flex flex-col sm:flex-row flex-wrap gap-4">
 
-            <PostLink 
-                v-for="(project, index) in projects.slice(0,2)"
-                :key="index"
-                type="project"
-                :title="project.title"
-                :link="project.link"
-                :thumbnail="projectThumb[project.thumbnail]"
-                :date="project.endDate"
-                :summary="project.summary"
-            /> 
+            <PostLink v-for="(project, index) in projects.slice(0, 2)" :key="index" type="project" :title="project.title"
+                :link="project.link" :thumbnail="projectThumb[project.thumbnail]" :date="project.endDate"
+                :summary="project.summary" />
         </div>
 
-        
+
     </Section>
 </template>
