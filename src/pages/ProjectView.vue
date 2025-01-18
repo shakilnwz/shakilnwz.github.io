6d6de8b814f5c6e19067d3fc46df7e5edd4edb09
Shakil Nawaz <shakilnwz@gmail.com>
2025-01-18 22:13:38 +0600

    updated tags

--- src/pages/ProjectView.vue
+++ src/pages/ProjectView.vue
@@ -14,15 +14,21 @@ const thumbs = { eliteabu, lifkoach, educavo }
     <Section contained>
         <h2 class="pb-6">Projects</h2>
         <div class="space-y-8 sm:space-y-16">
-            <a class="project-card flex h-fit flex-col relative sm:flex-row sm:even:flex-row-reverse gap-4 hover:border-brand-y/90 border-b-6 border-transparent rounded-3xl transition-all overflow-hidden"
-                v-for="(project, index) in projects" :key="index" :href="project.link" target="_blank">
+
+            <a class="project-card " v-for="(project, index) in projects" :key="index" :href="project.link"
+                target="_blank">
                 <span class="basis-full block rounded">
                     <img class="rounded-2xl h-full object-cover" :src="thumbs[project.thumbnail]" :alt="project.title">
                 </span>
-                <span class="basis-full space-y-4 px-2">
+                <span class="basis-full space-y-3 px-2">
                     <h3>{{ project.title }}</h3>
                     <p class="font-light text-brand-x/90 text-base">Started: {{ project.startDate }}, Ended: {{
                         project.endDate }}</p>
+                    <span class="project-tag">Tech/Tools Used:
+                        <ul>
+                            <li v-for="(tag, index) in project.tags" :key="index">{{ tag }} </li>
+                        </ul>
+                    </span>
                     <p>{{ project.summary }}</p>
                 </span>
             </a>
@@ -34,13 +40,29 @@ const thumbs = { eliteabu, lifkoach, educavo }
 @import "tailwindcss/theme" theme(reference);
 @import "../assets/styles/theme.css" theme(reference);
 
+
+
 .project-card {
+    @apply flex h-fit flex-col relative sm:flex-row sm:even:flex-row-reverse gap-4 hover:border-brand-y/90 border-b-6 border-transparent rounded-3xl transition-all overflow-hidden;
+
     &:before {
         @apply absolute text-center font-bold inset-full rounded-t-3xl bottom-0 text-transparent content-['View_Project'] font-bold py-4 transition-all;
     }
 
     &:hover:before {
         @apply text-brand-x inset-x-0 top-auto bg-brand-y/80 backdrop-blur-sm;
+    }
+
+    & .project-tag {
+        @apply text-base font-bold text-brand-y block;
+
+        & ul {
+            @apply flex flex-row flex-wrap;
+        }
+
+        & li {
+            @apply text-sm list-disc font-light ml-6 text-brand-x;
+        }
     }
 }
 
