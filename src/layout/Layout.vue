f36918f51562ebe4968a9988e94bdbd1f8c76dcf
Shakil Nawaz <shakilnwz@gmail.com>
2026-02-23 01:09:00 +0600

    wip: smooth scroll

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -3,18 +3,34 @@ import { provide } from 'vue';
 import { useRoute } from 'vue-router';
 import NavBar from '../components/templates/NavBar.vue';
 import PrimaryFooter from '../components/templates/PrimaryFooter.vue';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// ScrollSmoother requires ScrollTrigger
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
+ScrollSmoother.create({
+	smooth: 0.3, // how long (in seconds) it takes to "catch up" to the native scroll position
+	effects: true, // looks for data-speed and data-lag attributes on elements
+	smoothTouch: 0.1 // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+});
+
 
 const route = useRoute()
 provide('route', route)
 </script>
 <template>
     <NavBar />
-    <main 
-        :class="{
-            'h-vh overflow-y-auto sm:pt-16': true, 
-            'print:pt-0' : route.path === '/resume'
-        }">
-        <slot></slot>
-    </main>
-    <PrimaryFooter />
+    <div id="smooth-wrapper">
+        <div id="smooth-content">
+            <main 
+                :class="{
+                    'h-vh overflow-y-auto sm:pt-16': true, 
+                    'print:pt-0' : route.path === '/resume'
+                }">
+                <slot></slot>
+            </main>
+            <PrimaryFooter />
+        </div>
+    </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
-import deno from "@deno/vite-plugin";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno(), vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss()],
 });
