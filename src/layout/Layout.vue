74f5f41c9f859c7a35284e3f6d52ab655e92a0ce
Shakil Nawaz <shakilnwz@gmail.com>
2026-02-24 02:18:41 +0600

    wip: smooth scroll

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,19 +1,23 @@
 <script setup>
-import { provide } from 'vue';
+import { provide, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import NavBar from '../components/templates/NavBar.vue';
 import PrimaryFooter from '../components/templates/PrimaryFooter.vue';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-// ScrollSmoother requires ScrollTrigger
 import { ScrollSmoother } from "gsap/ScrollSmoother";
 
+onMounted(() => {
 gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
-ScrollSmoother.create({
-	smooth: 1.3, // how long (in seconds) it takes to "catch up" to the native scroll position
-	effects: true, // looks for data-speed and data-lag attributes on elements
-	smoothTouch: 0.1 // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+const smoother = ScrollSmoother.create({
+    wrapper: "#smooth-wrapper",
+    content: "#smooth-content",
+    smooth: 1.3,
+    effects: true,
+    smoothTouch: 0.1,
+    ignoreMobileResize: true
 });
+})
 
 
 const route = useRoute()
@@ -21,16 +25,18 @@ provide('route', route)
 </script>
 <template>
     <NavBar />
-    <div id="smooth-wrapper">
-        <div id="smooth-content">
-            <main 
-                :class="{
-                    'h-vh overflow-y-auto sm:pt-16': true, 
-                    'print:pt-0' : route.path === '/resume'
-                }">
+    <main 
+        :class="{
+            'sm:pt-16': true, 
+            'print:pt-0' : route.path === '/resume'
+        }">
+        <div id="smooth-wrapper" class="overscroll-none overflow-y-auto">
+            <div id="smooth-content">
                 <slot></slot>
-            </main>
-            <PrimaryFooter />
+            </div>
         </div>
-    </div>
+
+    </main>
+
+    <PrimaryFooter />
 </template>
