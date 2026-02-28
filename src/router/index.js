326dcaea43a66438d2a9341ba708504df12e4ae2
Shakil Nawaz <shakilnwz@gmail.com>
2026-02-28 23:29:49 +0600

    update: router

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,50 @@
-import { createWebHashHistory, createRouter } from "vue-router";
+import { createWebHistory, createRouter } from "vue-router";
 import HomeView from "../pages/home/IndexView.vue";
 
 const routes = [
-  {
-    path: "/",
-    children: [
-      {
-        path: "",
-        component: HomeView,
-      },
-      {
-        path: "/about",
+    {
+        path: "/",
         children: [
-          {
-            path: "",
-            component: () => import("../pages/about/IndexView.vue"),
-          },
-          {
-            path: "/resume",
-            component: () => import("../pages/about/ResumeView.vue"),
-          },
+            {
+                path: "",
+                component: HomeView,
+            },
+            {
+                path: "/about",
+                children: [
+                    {
+                        path: "",
+                        component: () => import("../pages/about/IndexView.vue"),
+                    },
+                    {
+                        path: "/resume",
+                        component: () => import("../pages/about/ResumeView.vue"),
+                    },
+                ],
+            },
+            {
+                path: "/articles",
+                component: () => import("../pages/articles/IndexView.vue"),
+            },
+            {
+                path: "/contact",
+                component: () => import("../pages/ContactView.vue"),
+            },
+            {
+                path: "/projects",
+                component: () => import("../pages/ProjectView.vue"),
+            },
+            {
+                path: "/wppost",
+                component: () => import("../pages/WPProjectsView.vue"),
+            },
         ],
-      },
-      {
-        path: "/articles",
-        component: () => import("../pages/articles/IndexView.vue"),
-      },
-      {
-        path: "/contact",
-        component: () => import("../pages/ContactView.vue"),
-      },
-      {
-        path: "/projects",
-        component: () => import("../pages/ProjectView.vue"),
-      },
-      {
-        path: "/wppost",
-        component: () => import("../pages/WPProjectsView.vue"),
-      },
-    ],
-  },
+    },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
